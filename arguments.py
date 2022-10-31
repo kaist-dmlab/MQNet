6c2459b825d2d9354f0a08fe28ced10f1cc2ad6a
Dongmin Park <pdm102207@gmail.com>
2022-10-31 13:24:09 +0900

    commit arguments.py

--- arguments.py
+++ arguments.py
@@ -10,9 +10,9 @@ parser.add_argument('--dataset', type=str, default='CIFAR10', help='dataset') #C
 parser.add_argument('--data_path', type=str, default='../data', help='dataset path')
 parser.add_argument('--gpu', default=None, nargs="+", type=int, help='GPU id to use')
 parser.add_argument("--data-parallel", default=False, type=str_to_bool, help="whether parallel or not")
-parser.add_argument('--ood-rate', type=float, default=0.6, metavar='N', help='rate of OOD over in')
+parser.add_argument('--ood-rate', type=float, default=0.6, metavar='N', help='OOD rate in unlabeled set')
 parser.add_argument('--n-class', type=str, default=10, help='# of classes')
-parser.add_argument('--trial', type=int, default=5, help='Random Seeds')
+parser.add_argument('--trial', type=int, default=5, help='# of runs')
 parser.add_argument('--cycle', type=int, default=10, help='# of AL cycles')
 parser.add_argument('--n-query', type=int, default=1000, help='# of query samples')
 parser.add_argument('--subset', type=int, default=50000, help='subset')
@@ -20,18 +20,15 @@ parser.add_argument('--resolution', type=int, default=32, help='resolution') # 3
 parser.add_argument('--model', type=str, default='ResNet18', help='model')
 parser.add_argument('--print_freq', '-p', default=300, type=int, help='print frequency (default: 20)')
 parser.add_argument('--seed', default=0, type=int, help="random seed")
-parser.add_argument('-j', '--workers', default=10, type=int, help='number of data loading workers (default: 4)')
+parser.add_argument('-j', '--workers', default=5, type=int, help='number of data loading workers (default: 4)')
 parser.add_argument("--ssl-save", default=True, type=str_to_bool, help="whether save ssl model or not")
 
 # Optimizer and scheduler
 parser.add_argument('--optimizer', default="SGD", help='optimizer to use, e.g. SGD, Adam')
 parser.add_argument('--lr', type=float, default=0.1, help='learning rate for updating network parameters')
 parser.add_argument('--lr-mqnet', type=float, default=0.001, help='learning rate for updating mqnet')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum (default: 0.9)')
-parser.add_argument('-wd', '--weight_decay', default=5e-4, type=float,
-                    metavar='W', help='weight decay (default: 5e-4)',
-                    dest='weight_decay')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum (default: 0.9)')
+parser.add_argument('-wd', '--weight_decay', default=5e-4, type=float, metavar='W', help='weight decay (default: 5e-4)', dest='weight_decay')
 parser.add_argument("--scheduler", default="MultiStepLR", type=str, help="Learning rate scheduler") #CosineAnnealingLR, StepLR, MultiStepLR
 parser.add_argument('--min_lr', type=float, default=1e-4, help='minimum learning rate for CosineAnnealingLR')
 parser.add_argument("--gamma", type=float, default=.5, help="Gamma value for StepLR")
@@ -45,8 +42,7 @@ parser.add_argument('--epoch-loss', default=120, type=int, help='number of epoch
 parser.add_argument('--epochs-ccal', default=700, type=int, help='number of epochs for training contrastive coders in CCAL')
 parser.add_argument('--epochs-csi', default=1000, type=int, help='number of epochs for training CSI')
 parser.add_argument('--epochs-mqnet', default=100, type=int, help='number of epochs for training mqnet')
-parser.add_argument('--steps-per-epoch', type=int, default=150, metavar='N',
-                    help='number of epochs to train (default: 200)')
+parser.add_argument('--steps-per-epoch', type=int, default=100, metavar='N', help='number of steps per epoch')
 parser.add_argument('--batch-size', "-b", default=64, type=int, metavar='N')
 parser.add_argument("--test-batch-size", "-tb", default=500, type=int)
 parser.add_argument('--ccal-batch-size', default=32, type=int, metavar='N')
